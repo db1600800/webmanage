a5ecef0d02ee5c5b22af20678b5b4d3391c7fa99
admin <admin@admintekiMacBook-Pro.local>
2018-02-09 16:39:37 +0800

    df
    dfd

---
+++ src/main/java/com/tools/mybatis/MybatisUtil.java
@@ -0,0 +1,26 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+import java.io.Reader;
+public class MybatisUtil {
+    private static SqlSessionFactory sessionFactory;
+    private static Reader reader;
+    static {
+        try {
+            //mybatis的配置文件
+            String resource = "mybatisConfig.xml";
+            //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+            //InputStream is = MybatisUtil.class.getClassLoader().getResourceAsStream(resource);
+            reader = Resources.getResourceAsReader(resource);
+            //构建sqlSession的工厂
+            sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static SqlSessionFactory getInstance() {
+        return sessionFactory;
+    }
+}
+
